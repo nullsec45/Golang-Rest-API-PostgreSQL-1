4ad3d7870e364e86d3854c622bdc59084ba2183a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-08-03 14:40:39 +0700

    feat : config

---
+++ internal/config/loader.go
@@ -0,0 +1,30 @@
+package config
+
+import(
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func Get() * Config{
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error when loading file configuration", err.Error())
+	}
+
+	return &Config{
+		Server:Server{
+			Host:os.Getenv("SERVER_HOST"),
+			Port:os.Getenv("SERVER_PORT"),
+		},
+		Database:Database{
+			Host:os.Getenv("DB_HOST"),
+			Port:os.Getenv("DB_PORT"),
+			Name:os.Getenv("DB_NAME"),
+			User:os.Getenv("DB_USER"),
+			Pass:os.Getenv("DB_PASS"),
+			Tz:os.Getenv("DB_TZ"),
+		},
+	}
+}
