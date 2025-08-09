1c2f1431b24dcb4b2ff7acc424be85d8045c1ce7
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-08-09 16:31:07 +0700

    feat : new validation

---
+++ internal/utility/validation.go
@@ -0,0 +1,28 @@
+package utility
+
+import (
+	"github.com/go-playground/validator/v10"
+	"fmt"
+	"strings"
+)
+
+func Validate[T any](data T)map[string]string {
+	err := validator.New().Struct(data)
+	res := map[string]string{}
+	if err != nil {
+		for _, v := range err.(validator.ValidationErrors){
+			res[v.StructField()] = TranslateTag(v)
+		}
+	}
+
+	return res
+}
+
+func TranslateTag(fd validator.FieldError) string {
+	switch fd.ActualTag(){
+	case "required" :
+		return fmt.Sprintf("Field %s wajib diisi", strings.ToLower(fd.StructField()))
+	}
+
+	return "validasi gagal"
+}
