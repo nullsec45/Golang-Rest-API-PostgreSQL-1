7ba1350c01efac81235cb9989b929df4a6872d5f
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-08-26 23:56:54 +0700

    feat : new journal

---
+++ domain/journal.go
@@ -0,0 +1,40 @@
+package domain 
+
+import (
+	"context"
+	"database/sql"
+	"github.com/nullsec45/Golang-Rest-API-PostgreSQL-1/dto"
+)
+
+const (
+	JOURNAL_STATUS_IN_PROGRESS="IN_PROGRESS"
+	JOURNAL_STATUS_COMPLETED="COMPLETED"
+)
+
+type Journal struct {
+	Id string `db:"id"`
+	BookId string `db:"book_id"`
+	StockCode string `db:"stock_code"`
+	CustomerId string `db:"customer_id"`
+	Status string `db:"status"`
+	BorrowedAt sql.NullTime `db:"borrowed_at"`
+	ReturnedAt sql.NullTime `db:"returned_at"`
+}
+
+type JournalSearch struct {
+	CustomerId string
+	Status string
+}
+
+type JournalRepository interface {
+	Find(ctx context.Context, se JournalSearch) ([]Journal, error)
+	FindById(ctx context.Context, id string) (Journal, error)
+	Save(ctx context.Context, journal *Journal) error
+	Update(ctx context.Context, journal *Journal) error
+}
+
+type JournalService interface {
+	Index(ctx context.Context, se JournalSearch) ([]dto.JournalData, error)
+	Create(ctx context.Context, req dto.CreateJournalRequest) error
+	Return(ctx context.Context, req dto.ReturnJournalRequest) error
+}
---
+++ internal/repository/journal.go
@@ -0,0 +1,51 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"github.com/doug-martin/goqu/v9"
+    "github.com/nullsec45/Golang-Rest-API-PostgreSQL-1/domain"
+)
+
+type JournalRepository struct {
+	db *goqu.Database
+}
+
+func NewJournal(con *sql.DB) domain.JournalRepository {
+	return &JournalRepository{
+		db:goqu.New("default", con),
+	}
+}
+
+func (j JournalRepository) Find(ctx context.Context, se domain.JournalSearch) (result []domain.Journal, err error){
+	dataset := j.db.From("journals")
+
+	if se.CustomerId != "" {
+		dataset = dataset.Where(goqu.C("customer_id").Eq(se.CustomerId))
+	}
+
+	if se.Status != "" {
+		dataset = dataset.Where(goqu.C("status").Eq(se.Status))
+	}
+
+	err = dataset.ScanStructsContext(ctx, &result)
+	return
+}
+
+func (j JournalRepository) FindById(ctx context.Context, id string) (result domain.Journal, err error) {
+	dataset := j.db.From("journals").Where(goqu.C("deleted_at").IsNull(), goqu.C("id").Eq(id))
+	_, err = dataset.ScanStructContext(ctx, &result)
+	return
+}
+
+func (j JournalRepository) Save (ctx context.Context, journal *domain.Journal) error {
+	executor := j.db.Insert("journals").Executor()
+	_, err := executor.ExecContext(ctx)
+	return err
+}
+
+func (j JournalRepository) Update (ctx context.Context, journal *domain.Journal) error {
+	 executor := j.db.Update("journals").Set(journal).Where(goqu.C("id").Eq(journal.Id)).Executor()
+    _, err := executor.ExecContext(ctx)
+    return err
+}
