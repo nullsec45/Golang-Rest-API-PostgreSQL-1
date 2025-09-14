0da755a7c65721064c43ca2d5038e1c1c091183a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-09-14 22:49:10 +0700

    migration : table users.

---
+++ database/migrations/20250914150732_create_table_users.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "public"."users" (
+    "id"  UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "email" character varying(255) NOT NULL,
+    "password" character varying(255) NOT NULL
+);
